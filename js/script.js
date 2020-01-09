d21045cf1b06372c8b559b40d7c59bac6ff36c92
Marco <marcopa84@hotmail.it>
2020-01-09 15:49:16 +0100

    completato anche ciclo while e stampato

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ for (var i = 1; i <= 1000; i++) {
     document.getElementById('lista_metodo_for').innerHTML += '<li>' + i + '</li>'
   }
 }
+
+var i =1;
+while (i <= 1000) {
+  if (i % 3 == 0 && i % 5 == 0){
+    console.log('FizzBuzz');
+    document.getElementById('lista_metodo_while').innerHTML += '<li>' + i + ' is ' + 'FizzBuzz' + '</li>'
+  }
+  else if (i % 5 == 0){
+    console.log('Buzz');
+    document.getElementById('lista_metodo_while').innerHTML += '<li>' + i + ' is '+ 'Buzz' + '</li>'
+  }
+  else if (i % 3 == 0){
+    console.log('Fizz');
+    document.getElementById('lista_metodo_while').innerHTML += '<li>' + i + ' is ' + 'Fizz' + '</li>'
+  }
+  else {
+    console.log(i);
+    document.getElementById('lista_metodo_while').innerHTML += '<li>' + i + '</li>'
+  }
+  i++
+}
