a2966522458ff6cd6f84914361cbfaac69eb56ac
Marco <marcopa84@hotmail.it>
2020-01-09 15:08:02 +0100

    commit iniziale

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// alert('ciao');
+// var numero = 0;
+
+for (var i = 0; i <= 1000; i++) {
+  if (i/3){
+    console.log('Fizz');
+  }
+  else if (i/5){
+    console.log('Buzz');
+  }
+  else if (i/15){
+    console.log('FizzBuzz');
+  }
+  else {
+    console.log(i);
+  }
+}
