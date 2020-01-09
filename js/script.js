b9c8af4d246195a06da91d514e66987c16beec48
Marco <marcopa84@hotmail.it>
2020-01-09 15:46:25 +0100

    CICLO FOR TERMINATO E STAMPATO IN HTML

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 // alert('ciao');
 // var numero = 0;
 
-for (var i = 0; i <= 1000; i++) {
-  if (i/3){
-    console.log('Fizz');
-  }
-  else if (i/5){
-    console.log('Buzz');
-  }
-  else if (i/15){
+for (var i = 1; i <= 1000; i++) {
+
+  if (i % 3 == 0 && i % 5 == 0){
     console.log('FizzBuzz');
+    document.getElementById('lista_metodo_for').innerHTML += '<li>' + i + ' is ' + 'FizzBuzz' + '</li>'
+  }
+  else if (i % 5 == 0){
+    console.log('Buzz');
+    document.getElementById('lista_metodo_for').innerHTML += '<li>' + i + ' is '+ 'Buzz' + '</li>'
+  }
+  else if (i % 3 == 0){
+    console.log('Fizz');
+    document.getElementById('lista_metodo_for').innerHTML += '<li>' + i + ' is ' + 'Fizz' + '</li>'
   }
   else {
     console.log(i);
+    document.getElementById('lista_metodo_for').innerHTML += '<li>' + i + '</li>'
   }
 }
